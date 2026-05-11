41df8cd9f3f0bd723622073931e39da9541ed3be
Daniel Casalli <danis.settk@gmail.com>
2026-05-11 14:37:47 -0300

    feat: inicia tela inicial

--- olhos-dagua/src/screens/Inicio.js
+++ olhos-dagua/src/screens/Inicio.js
@@ -1,1 +1,4 @@
-.
+import React from 'react';
+import { View, StyleSheet, ScrollView, TouchableOpacity, Image, FlatList } from 'react-native';
+import { Title, Paragraph, Chip } from 'react-native-paper';
+import { Ionicons } from '@expo/vector-icons';
