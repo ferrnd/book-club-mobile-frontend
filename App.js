17bd6a98d17d17182638c704c924393d14067237
ferrnd <sombarepam@gmail.com>
2026-05-12 17:30:18 -0300

    feat: implementa integração com a API para buscar e exibir dados dos livros

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  ActivityIndicator,
+  Image,
+  StyleSheet,
+} from "react-native";
+
+// {fernando 15:46 } testando a conexão com a API usando fetch
+
+const API_URL = "https://olhosdagua.onrender.com/api/livro";
+const API_KEY =
+  "6uztY7YTa2Dcgnf2ovDC2Kqmwvq2PdTMOlkx1bLwmhO2HQpQoXHMhk1cBcIjzHj9lztTbW7I83UZ91C8uSos-n8kOx3UuqU8n0BIDVm1venccSH0QVyNYKkLTZboaUpd";
 
 export default function App() {
+  const [livros, setLivros] = useState([]);
+  const [carregando, setCarregando] = useState(true);
+
+  async function buscarLivros() {
+    try {
+      const resposta = await fetch(API_URL, {
+        headers: { "x-api-key": API_KEY },
+      });
+      const dados = await resposta.json();
+      setLivros(dados);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setCarregando(false);
+    }
+  }
+
+  useEffect(() => {
+    buscarLivros();
+  }, []);
+
+  if (carregando) {
+    return <ActivityIndicator />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={livros}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Image source={{ uri: item.capa }} style={styles.foto} />
+            <Text>--------------------------------------------</Text>
+            <Text>Título: {item.titulo}</Text>
+            <Text>--------------------------------------------</Text>
+            <Text>Autor: {item.autor}</Text>
+            <Text>--------------------------------------------</Text>
+            <Text>ano de publicação: {item.anoPublicacao}</Text>
+            <Text>--------------------------------------------</Text>
+            <Text>Resumo: {item.resumo}</Text>
+            <Text>--------------------------------------------</Text>
+            <Text>resume: {item.resumo_en}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 20,
+  },
+
+  foto: {
+    width: 100,
+    height: 150,
   },
 });
