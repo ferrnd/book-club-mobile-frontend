72e019dd24eaeb0e16907f7ca93e9f33c063be73
ferrnd <sombarepam@gmail.com>
2026-05-14 15:07:21 -0300

    feat: configurei fontes externas (Nunito e Outfit) com expo-google-fonts
    - Mudei o tipo de arquivo também (de app.js virou app.jsx)

---
+++ App.jsx
@@ -0,0 +1,39 @@
+import { View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+import { 
+  useFonts, 
+  Nunito_400Regular, 
+  Nunito_400Regular_Italic, 
+  Nunito_700Bold 
+} from "@expo-google-fonts/nunito";
+
+import { 
+  Outfit_400Regular, 
+  Outfit_700Bold, 
+  Outfit_800ExtraBold 
+} from "@expo-google-fonts/outfit";
+
+import TelaInicial from "./src/screens/TelaInicial.jsx";
+
+export default function App() {
+
+  const [carregado] = useFonts({
+    Nunito_400Regular,
+    Nunito_400Regular_Italic,
+    Nunito_700Bold,
+    Outfit_400Regular,
+    Outfit_700Bold,
+    Outfit_800ExtraBold,
+  });
+
+  // se a fonte ainda n carregou n mostra nd
+  if (!carregado) return null;
+
+  return (
+    <View style={{ flex: 1 }}>
+      <StatusBar style="light" />
+      <TelaInicial />
+    </View>
+  );
+}
